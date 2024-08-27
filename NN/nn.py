fb02321a024ca8aa03e32b919b0b77dad0fa8868
yuutan1017 <n.you0611@gmail.com>
2024-08-27 22:12:39 +0900

    mod: save

--- NN/nn.py
+++ NN/nn.py
@@ -1,12 +1,19 @@
 import numpy as np
+import matplotlib.pylab as plt
 
 
-def step_func(x) -> int:
-    if x > 0:
-        return 1
-    else:
-        return 0
+def step_func(x):
+    return np.array(x > 0, dtype=np.int64)
+
+
+def sub_main() -> None:
+    x = np.arange(-5.0, 5.0, 0.1)
+    y = step_func(x)
+
+    plt.plot(x, y)
+    plt.ylim(-0.1, 1.1)
+    plt.show()
 
 
 if __name__ == "__main__":
-    pass
+    sub_main()
